e4068ba218ef4ba1c99f42d2af93d228d372613e
PABLOPARRALOPEZ <107953420+PABLOPARRALOPEZ@users.noreply.github.com>
2022-08-06 11:56:25 -0600

    Add files via upload

---
+++ grid.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("div.block").click(function(){
+    $(this).css("background-color", "#989899");
+    }, function(){
+    $(this).css("background-color", "#9c9c9c");
+  });
+});
